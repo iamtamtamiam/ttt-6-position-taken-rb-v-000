3f32e810279800ef46c856d00bf78530b269fa8d
Tam Vu <vutam_a@hotmail.com>
2018-09-09 17:08:44 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,9 +2,6 @@
 
 def position_taken?(board, index)
   board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-  if board[index] == "X" || board[index] == "O"
-    true
-  else 
-    false
-  end
-end
+ 
+ end
+ 
